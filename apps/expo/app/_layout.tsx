36875e28a2d0d111d00829dc264785a766cab8d7
Brenden Smith <git@brenden-smith.com>
2023-03-21 04:17:26 -0700

    Fix status bar

--- apps/expo/app/_layout.tsx
+++ apps/expo/app/_layout.tsx
@@ -1,9 +1,11 @@
 import { Provider } from 'app/provider'
 import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
 export default function Root() {
   return (
     <Provider>
+      <StatusBar style="dark" />
       <Stack />
     </Provider>
   )
